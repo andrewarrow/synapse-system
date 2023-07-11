7dcf3572e9efdb7c8babaab3e6c4f8c24e8df82b
Andrew Arrow <oneone@gmail.com>
2023-07-11 12:49:56 -0700

    sleep

--- python/run.py
+++ python/run.py
@@ -3,14 +3,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import sys
+import time
+import json
 
 email = sys.argv[1]
 
 options = Options()
-options.add_argument('-headless')
+#options.add_argument('-headless')
 #options.headless = True
 browser = webdriver.Firefox(options=options)
 browser.get('https://manypw.slack.com/')
 
 elem = browser.find_element(By.NAME, 'email')
 elem.send_keys(email + Keys.RETURN)
+
+time.sleep(26)
+
+local_storage_data = browser.execute_script("return window.localStorage;")
+session_cookies = browser.get_cookies()
+
+browser.quit()
+print("writing")
+
+json_data = json.dumps(local_storage_data)
+
+with open('local_storage_data.txt', 'w') as file:
+    file.write(json_data)
+
+with open('session_cookies.txt', 'w') as file:
+    for cookie in session_cookies:
+        file.write(f"{cookie['name']}={cookie['value']}\n")
+
+print("done")
