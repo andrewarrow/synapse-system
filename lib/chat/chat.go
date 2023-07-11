61cb7f5b8dfe5afc6879ec4f973ebf626056894a
Andrew Arrow <oneone@gmail.com>
2023-07-11 16:52:41 -0700

    geckodriver.log

--- lib/chat/chat.go
+++ lib/chat/chat.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"fmt"
+	"os/exec"
 	"strings"
 
 	"github.com/andrewarrow/feedback/router"
@@ -11,9 +12,9 @@ func Run(r *router.Router) {
 	users := r.All("user", "order by created_at", "")
 	for _, user := range users {
 		fullName := user["full_name"].(string)
-		fmt.Println(fullName)
 		path := firstNameLower(fullName)
 		fmt.Println(fullName, path)
+		go turnGreenLightOn(path)
 		token := user["slack_token"].(string)
 		slackUser := user["slack_user"].(string)
 		_, _ = token, slackUser
@@ -21,6 +22,11 @@ func Run(r *router.Router) {
 		//slack.RtmConnect(token, slackUser)
 		//slack.PostMessage(token, "C05G01UFYMU", "test")
 	}
+}
+
+func turnGreenLightOn(path string) {
+	b, err := exec.Command("python3", "python/online.py", "python/"+path, "26").CombinedOutput()
+	fmt.Println(string(b), err)
 }
 
 func firstNameLower(s string) string {
