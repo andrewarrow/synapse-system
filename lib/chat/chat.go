56f360f97db1c9be040c1126d0bab0504e0aec0a
Andrew Arrow <oneone@gmail.com>
2023-07-11 15:07:04 -0700

    path

--- lib/chat/chat.go
+++ lib/chat/chat.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/andrewarrow/feedback/router"
 )
@@ -11,6 +12,8 @@ func Run(r *router.Router) {
 	for _, user := range users {
 		fullName := user["full_name"].(string)
 		fmt.Println(fullName)
+		path := firstNameLower(fullName)
+		fmt.Println(fullName, path)
 		token := user["slack_token"].(string)
 		slackUser := user["slack_user"].(string)
 		_, _ = token, slackUser
@@ -18,4 +21,9 @@ func Run(r *router.Router) {
 		//slack.RtmConnect(token, slackUser)
 		//slack.PostMessage(token, "C05G01UFYMU", "test")
 	}
+}
+
+func firstNameLower(s string) string {
+	tokens := strings.Split(s, " ")
+	return strings.ToLower(tokens[0])
 }
