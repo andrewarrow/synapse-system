929bd7fae0b34449af2ff19ef3cdf44626536129
Andrew Arrow <oneone@gmail.com>
2023-07-11 12:01:45 -0700

    tabs

--- lib/cookies/parse.go
+++ lib/cookies/parse.go
@@ -3,10 +3,20 @@ package cookies
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func Parse(filename string) {
 	b, _ := ioutil.ReadFile(filename)
 	s := string(b)
-	fmt.Println(len(s))
+	lines := strings.Split(s, "\n")
+	for _, line := range lines {
+		tokens := strings.Split(line, "\t")
+		if len(tokens) == 1 {
+			continue
+		}
+		name := tokens[0]
+		value := tokens[1]
+		fmt.Println(name, value)
+	}
 }
